a221bff98a7d3f1dea134aa4208aadf329262c5e
mynameiszp <nastyazaporozhchenko@gmail.com>
2023-09-11 16:49:14 +0300

    feat: implementes fall animation for otter player, some changes in tilemaps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -9,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpLength = 3f;
     [SerializeField] float jumpHeigth = 5f;
 
-    CapsuleCollider2D collider;
-    Animator animator;
-    Rigidbody2D rigidbody;
+    private CapsuleCollider2D collider;
+    private Animator animator;
+    private Rigidbody2D rigidbody;
+    private float waitBeforeReload = 0.1f;
+    private bool isAlive = true;
 
     void Start()
     {
@@ -22,18 +25,31 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (isAlive)
+        {
+            StartCoroutine(Fall());
+        }
+    }
 
+    IEnumerator Fall()
+    {
+        if (collider.IsTouchingLayers(LayerMask.GetMask("Bottom")))
+        {
+            animator.SetTrigger("hasFallen");
+            isAlive = false;
+            yield return new WaitForSecondsRealtime(waitBeforeReload);
+            //reload level
+        }
     }
 
     IEnumerator OnJump(InputValue input)
     {
-        if (input.isPressed && collider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        if (input.isPressed && collider.IsTouchingLayers(LayerMask.GetMask("Ground")) && isAlive)
         {
             animator.SetBool("isJumping", input.isPressed);
             rigidbody.velocity = new Vector2(jumpLength, jumpHeigth);
             yield return new WaitForSecondsRealtime(animator.GetCurrentAnimatorClipInfo(0)[0].clip.length);
             animator.SetBool("isJumping", false);
         }
-
     }
 }
