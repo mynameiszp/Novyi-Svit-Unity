340ea4160137330aa36d3a95ddb3508cee6dc5ee
mynameiszp <nastyazaporozhchenko@gmail.com>
2023-09-14 19:53:35 +0300

    feat: added ui for pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnPause()
+    {
+
+    }
+
+    public void OnResume()
+    {
+
+    }
+    public void OnSettings()
+    {
+
+    }
+    public void OnMainMenu()
+    {
+
+    }
+
+
+}
