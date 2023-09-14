4ed4eeb99a73b1003314a086ed92362021708de9
mynameiszp <nastyazaporozhchenko@gmail.com>
2023-09-14 21:16:35 +0300

    feat: implemented pausing and resuming mechanism

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,36 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
+    [SerializeField] GameObject pauseScreen;
+
     void Start()
     {
-
-    }
-
-    void Update()
-    {
-
+        pauseScreen.SetActive(false);
     }
 
     public void OnPause()
     {
-
+        Time.timeScale = 0;
+        pauseScreen.SetActive(true);
     }
 
     public void OnResume()
     {
-
+        Time.timeScale = 1;
+        pauseScreen.SetActive(false);
     }
+
     public void OnSettings()
     {
 
     }
+
     public void OnMainMenu()
     {
-
+        //save current position and data
+        SceneManager.LoadScene(0);
     }
-
-
 }
